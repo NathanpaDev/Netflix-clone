a469421b3cd55808dc942723ad8fb722f208dcef
Natanael Romero de Assunção <nathanpadev@gmail.com>
2025-05-15 17:32:44 -0300

    Adicionando arquivos iniciais

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+// eslint-disable-next-line import/no-anonymous-default-export
+export default () => {
+  return (
+    <div>
+      <h1>Welcome to React</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  );
+} 
